03a23835f7380261b404b638e189d1f883c181c4
NICHTJ3 <nicholsontrent@gmail.com>
2020-02-29 20:35:20 +1300

    Setup eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import bodyParser from "body-parser";
-import express, { Application, Request, Response } from "express";
+import bodyParser from 'body-parser';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
 app.use(
   bodyParser.urlencoded({
-    extended: false
-  })
+    extended: false,
+  }),
 );
 app.use(bodyParser.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to using typescript" });
+app.get('/', (_: Request, res: Response) => {
+  res.json({ message: 'Welcome to using typescript' });
 });
 
 export default app;
